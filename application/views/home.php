65e0d1f060bbe9a167383e22ff1164cd8223db4d
s0fy4n002 <s0fy4n002@gmail.com>
2022-08-08 12:57:49 +0700

    note

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,21 @@
-<h1>Home Page</h1>
+<div class="row">
+	<div class="col-md-12">
+		<div class="card">
+			<div class="card-body">
+				<button class="btn btn-primary">Tambah</button>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>Nama</th>
+							<th>Image</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr></tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
